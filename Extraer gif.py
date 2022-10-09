c144075b48a2b290e4d01887f19b04ea3a950f2f
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-10-09 03:23:57 +0200

    Add files via upload

---
+++ Extraer gif.py
@@ -0,0 +1,29 @@
+
+from PIL import Image, ImageSequence
+import os
+import pathlib
+ 
+
+
+NombreArchivoGif = input("Escribe el nombre del gif: ")
+im = Image.open(f"{NombreArchivoGif}")
+ 
+
+Carpeta= input("Escribe el nombre de la carpeta: ")
+pathlib.Path(f"{Carpeta}").mkdir(parents=True, exist_ok=True)
+os.chdir(os.path.join(os.getcwd(),f"{Carpeta}")) 
+
+
+i =1
+
+
+ 
+
+app = []
+ 
+for fr in ImageSequence.Iterator(im):
+    app.append(fr)
+    fr.save("%d.png"%i)
+    i = i + 1
+ 
+
