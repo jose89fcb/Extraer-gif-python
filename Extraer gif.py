80729c14eb3f3e38ab6309241905c346963f5010
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-10-28 05:04:01 +0200

    Update Extraer gif.py

--- Extraer gif.py
+++ Extraer gif.py
@@ -17,13 +17,15 @@ os.chdir(os.path.join(os.getcwd(),f"{Carpeta}"))
 i =1
 
 
- 
+
 
 app = []
- 
+
 for fr in ImageSequence.Iterator(im):
     app.append(fr)
-    fr.save("%d.png"%i)
+  
+    fr.save("%d.gif"%i)
+    print("imagen_%d.png"%i)
+    
     i = i + 1
  
-
